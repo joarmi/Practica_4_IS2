fed9b5401cad5751394348d1823a336a87b3ef0b
joarmi <javiondogamer@gmail.com>
2016-11-17 11:57:33 +0100

    Version Inicial
    Comienzo de la practica

---
+++ JUnit/src/junit/ProductNotFoundException.java
@@ -0,0 +1,21 @@
+package dominio;
+/**
+ * Exception thrown when a product is not found in a shopping cart.
+ * @author Mike Clark
+ * @author Clarkware Consulting, Inc. 
+ */
+ 
+public class ProductNotFoundException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructs a ProductNotFoundException.
+	 */
+	public ProductNotFoundException() {
+		super();
+	}
+}
